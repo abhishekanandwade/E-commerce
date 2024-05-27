18dc4b442ee16214715e0f965ac9017ecec6a68f
Abhishek <abhi.anandwade@gmail.com>
2024-05-27 17:45:33 +0530

    chore: Update dependencies, add Firebase configuration, and implement Google authentication

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD3V95kMe95oPvGwwWrRngy6qIVQq50gjs",
+    authDomain: "amzn-clone-5d076.firebaseapp.com",
+    projectId: "amzn-clone-5d076",
+    storageBucket: "amzn-clone-5d076.appspot.com",
+    messagingSenderId: "724157947960",
+    appId: "1:724157947960:web:c3cc496de2031b2d5106cd",
+    measurementId: "G-H4CJVX7RTV"
+  };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { SearchIcon, ShoppingCartIcon, MenuIcon } from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
 function Header() {
     return (
         <header>
@@ -23,7 +24,7 @@ function Header() {
                 </div>
                 {/* Right */}
                 <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-                    <div className="link">
+                    <div onClick={signIn} className="link">
                         <p>Hello Abhishek</p>
                         <p className="font-extrabold md:text-sm">Account & Lists</p>
                     </div>
