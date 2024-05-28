75034f93b974e110decc438f3d19ee26ce006098
Abhishek <abhi.anandwade@gmail.com>
2024-05-28 22:06:03 +0530

    chore: Add lazy loading for images and implement checkout page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import { SearchIcon, ShoppingCartIcon, MenuIcon } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
 function Header() {
+
+    const { data: session } = useSession()
+    const router = useRouter()
+
     return (
         <header>
             {/* top Nav */}
             <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
                 <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
                     <Image
+                        onClick={() => router.push('/')}
                         src="https://links.papareact.com/f90"
                         width={150}
                         height={40}
@@ -24,15 +31,15 @@ function Header() {
                 </div>
                 {/* Right */}
                 <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-                    <div onClick={signIn} className="link">
-                        <p>Hello Abhishek</p>
+                    <div onClick={!session ? signIn : signOut} className="link">
+                        {session ? <p>Hello, {session.user.name}</p> : <p>Sign In</p>}
                         <p className="font-extrabold md:text-sm">Account & Lists</p>
                     </div>
                     <div className="link">
                         <p>Returns</p>
                         <p className="font-extrabold md:text-sm">& Orders</p>
                     </div>
-                    <div className="relative link flex items-center">
+                    <div onClick={()=> router.push('/checkout')} className="relative link flex items-center">
                         <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold" >0</span>
                         <ShoppingCartIcon className="h-10" />
                         <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
@@ -43,7 +50,7 @@ function Header() {
             {/* Bottom Nav */}
             <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
                 <p className="link flex items-center">
-                    <MenuIcon className="h-6 mr-1"/>
+                    <MenuIcon className="h-6 mr-1" />
                     All
                 </p>
                 <p className="link" >Prime Video</p>
